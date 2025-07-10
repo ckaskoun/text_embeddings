103d1e0a148ec20cdf56ea7b1948bb6860f187dc
ckaskoun <carlos.kaskoun@gmail.com>
2025-07-10 16:17:48 -0400

    temporary skeleton from GEVIR repo

--- create_embeddings.py
+++ create_embeddings.py
@@ -5,3 +5,20 @@ import numpy as np
 
 # Import embeddings model
 model = AutoModel.from_pretrained("jinaai/jina-embeddings-v2-small-en", trust_remote_code=True)
+
+# Load dataset
+with open("data/PERC2001-2023_ExtraArticles/processed_text.pkl", "rb") as f:
+    df = pkl.load(f)
+df.head()
+
+%%time
+def encode_text(row):
+    text = row['raw']
+    embedding = model.encode(text)
+    return embedding.tolist()
+
+# Apply the function along the rows and assign the result to the new 'embedding' column
+df['embedding'] = df.apply(encode_text, axis=1)
+
+with open("../data/PERC2001-2023_ExatraArticles/embeddings_jina.pkl", "wb") as f:
+    pkl.dump(df, f)
