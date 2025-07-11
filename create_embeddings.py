9cc8942f26a926781676c8d0fe48e1201a35a008
ckaskoun <carlos.kaskoun@gmail.com>
2025-07-11 10:56:37 -0400

    Updated create_embeddings file, combined with extract_sentences

--- create_embeddings.py
+++ create_embeddings.py
@@ -1,28 +1,88 @@
-# Importing the libraries
-# from transformers import AutoModel
-import pickle as pkl
-import numpy as np
 import os
+import re
 import pandas
-from extract_sentences import clean_documents
+from transformers import AutoModel
 
-# Import embeddings model
-# model = AutoModel.from_pretrained("jinaai/jina-embeddings-v2-small-en", trust_remote_code=True)
+def clean_documents(folder_path):
+    """
+    Cleans .xlsx files from a folder for use with LLMs.
 
-# Clean data in files
-filenames = []
-folder_path = 'C:/Users/carlo/OneDrive/Documents/memos_excel'
-for file in os.listdir(folder_path):
-    path = os.path.join(folder_path, file)
-    filenames.append(path)
+    Arguments:
+    <folder>: path to a folder containing >= 1 .xlsx file with the first two
+              columns being numbers 0, 1, 2, ... and text.
 
-df = clean_documents(filenames)
-print(df)
+    Returns:
+    <cleaned_docs>: a dict of pandas dfs with one column, 'Sentences', with
+                    indices matching line numbers in the first column of the .xlsx file.
+                    Dictionary keys are the file names without extension.
+    """
+    # Retrieve file paths
+    file_names = []
+    for file_path in os.listdir(folder_path):
+        path = os.path.join(folder_path, file_path)
+        file_names.append(path)
+
+    cleaned_docs = {}
+
+    for path in file_names:
+        # Find base name
+        name = os.path.basename(path)
+        name = os.path.splitext(name)[0]
+        # Remove number column and newline characters
+        df = pandas.read_excel(path)
+        df.drop(columns='Unnamed: 0', inplace=True)
+        for sentence in df['Sentences']:
+            if '\n' in sentence:
+                cleaned_sentence = re.sub(r'\n+', ' ', sentence)
+                cleaned_sentence = cleaned_sentence.strip('\n')
+                df['Sentences'] = df['Sentences'].replace([sentence], cleaned_sentence)
+        cleaned_docs[name] = df
+
+    return cleaned_docs
 
 def encode_text(row):
-    text = row['Sentences']
+    """
+    Reads the text within a pandas df in a specific column and per row,
+    creates an embedding using the assigned model.
+
+    > This function requires a pre-defined model and column name
+      before it is called.
+
+    Arguments:
+    <row>: For applying to each row of a pandas df.
+
+    Returns:
+    A list of the coordinates of the embeddings.
+    """
+    text = row[column]
     embedding = model.encode(text)
     return embedding.tolist()
 
-# Apply the function along the rows and assign the result to the new 'embedding' column
-# df['embedding'] = df.apply(encode_text, axis=1)
+def embed_files(folder_path, embed_model="jinaai/jina-embeddings-v2-small-en"):
+    """
+    Embeds text within a .xlsx file.
+
+    Attributes:
+    <folder_path>: Path to a folder containing only .xlsx files in the proper format.
+    <embed_model>: The model to be used to embed the text. Default is Jina v2.
+
+    Returns:
+    <df_dict>: A dictionary containing filenames without extension as keys and
+               dataframes for each file contining a 'Sentence' column with text
+               and an 'Embeddings' column containing the embeddings.
+    """
+    # Define variables needed for encode_text function
+    global model, column
+    model = AutoModel.from_pretrained(embed_model, trust_remote_code=True)
+    column = 'Sentences'
+
+    # Clean .xlsx files
+    df_dict = clean_documents(folder_path)
+
+    # Apply encode_text function to sentences for each file, update df
+    for k in df_dict:
+        df = df_dict[k]
+        df['Embeddings'] = df.apply(encode_text, axis=1)
+        df_dict[k] = df
+
+    return df_dict
